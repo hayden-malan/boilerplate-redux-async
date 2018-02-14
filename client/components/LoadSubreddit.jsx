33c82a434b5ecc69e0f65385a59615a4418e14d0
edi-rose <edirose1998@gmail.com>
2018-02-14 17:18:45 +1300

    step 2

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -2,10 +2,39 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {fetchPosts} from '../actions'
 
-const LoadSubreddit = ({dispatch}) => (
-  <button onClick={() => dispatch(fetchPosts('newzealand'))}>
-    Fetch Posts
-  </button>
-)
+
+
+
+class LoadSubreddit extends React.Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+}
+
+render() {
+  return (
+    <div>
+      <input type='text'
+       value={this.state.subreddit}
+       onChange={(e) => this.setTarget(e)} />
+     <button
+       onClick={() => this.props.dispatch(fetchPosts(this.state.subreddit))}
+>Fetch Posts</button>
+</div>
+  )
+ }
+
+
+  const LoadSubreddit = ({dispatch}) => (
+<div>
+    <input type='text' name='subreddit' />
+
+    <button onClick={() => dispatch(fetchPosts('newzealand'))}>
+      Fetch Posts
+    </button>
+</div>
+  )
 
 export default connect()(LoadSubreddit)
